9f2d904f76221ee72edd0cd500584178785caf15
Tamara <alvarezht23@gmail.com>
2023-02-28 20:47:13 -0300

    foto v3

--- cv_tamara_alvarez.js
+++ cv_tamara_alvarez.js
@@ -3,14 +3,16 @@ let fotito = document.getElementById("img_perfil"); //selecciono la foto de perf
 
 let elementocheckbox = document.querySelector('#switch input[type="checkbox"]'); //selecciono el checkbox
 
+//funcion cambiarfoto, cambia la img de perfil segun el checkbox. 
+//(En el live server en mi pc me funciona bien, en github tambien pero cuando lo subi a githubpages no me carga la 2da imagen)
 function cambiarfoto(e){
     if(e.target.checked){
 
-        fotito.src="./imagenes/yuu.png"
+        fotito.src="imagenes/yuu.png"
         }
         else {
           
-            fotito.src="./imagenes/nunu.png"
+            fotito.src="imagenes/nunu.png"
         }
     }
         elementocheckbox.addEventListener('change', cambiarfoto);
