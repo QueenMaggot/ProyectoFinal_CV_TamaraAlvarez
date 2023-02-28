c1f418aa8d581f2b851a30944797686b7ef9fccc
Tamara <alvarezht23@gmail.com>
2023-02-28 20:33:07 -0300

    arregle img perfil

--- cv_tamara_alvarez.js
+++ cv_tamara_alvarez.js
@@ -6,11 +6,11 @@ let elementocheckbox = document.querySelector('#switch input[type="checkbox"]');
 function cambiarfoto(e){
     if(e.target.checked){
 
-        fotito.src="/imagenes/yuu.png"
+        fotito.src="./imagenes/yuu.png"
         }
         else {
           
-            fotito.src="/imagenes/nunu.jpg"
+            fotito.src="./imagenes/nunu.jpg"
         }
     }
         elementocheckbox.addEventListener('change', cambiarfoto);
