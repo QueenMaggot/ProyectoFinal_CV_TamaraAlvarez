de5a3f651bc5b7f9644d42165c9fe9cb7f68c5ed
Tamara <alvarezht23@gmail.com>
2023-02-28 18:35:54 -0300

    agregue funcion js para cambiar foto de perfil, cambios de colores en css

---
+++ cv_tamara_alvarez.js
@@ -0,0 +1,16 @@
+
+let fotito = document.getElementById("img_perfil"); //selecciono la foto de perfil
+
+let elementocheckbox = document.querySelector('#switch input[type="checkbox"]'); //selecciono el checkbox
+
+function cambiarfoto(e){
+    if(e.target.checked){
+
+        fotito.src="/imagenes/yuu.png"
+        }
+        else {
+          
+            fotito.src="/imagenes/nunu.jpg"
+        }
+    }
+        elementocheckbox.addEventListener('change', cambiarfoto);
